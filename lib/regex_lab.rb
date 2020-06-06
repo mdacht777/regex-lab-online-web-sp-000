545171609650cdaeeb95eff6fd0d4b42ebb222b5
Martin Achtermann <mdacht777@gmail.com>
2020-06-06 15:28:24 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/[(]*[0-9]{3}[ )-]*[0-9]{3}[-]*[0-9]{4}\b/)
+  if  phone.scan(/[(]*[0-9]{3}[ )-]*[0-9]{3}[-]*[0-9]{4}\b/)
+    return true
+  else
+    return FALSE
+  end
 end
